6c69aae8ba501bda0573c45f65b62a78f7f216c2
guizinpow <106358613+guizinpow@users.noreply.github.com>
2023-10-09 22:19:50 -0300

    0910

---
+++ css/main.js
@@ -0,0 +1,12 @@
+const robotron = document.querySelector("#robotron")
+robotron.addEventListener("click", function {
+    console.log('cliquei no robo')
+    
+})
+
+function dizOi() {
+    console.log('Oi')
+    console.log("Bem-vindo ao Robotron 2000")
+}
+
+dizOi()
